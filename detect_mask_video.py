8779546b443933ecb406f722fd46ec38f125aacc
HENRY <73129698+YaduvanshiHenry@users.noreply.github.com>
2020-12-03 16:07:11 +0530

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -1,5 +1,5 @@
+#For Real-Time Scanning
 # import the necessary packages
-
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
